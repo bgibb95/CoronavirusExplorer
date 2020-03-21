e35c5f9b2f0217f7f8fa97c24237b526482caa1b
Brendan <b.gibb@live.co.za>
2020-03-21 11:00:06 +0200

    Deploy config

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+      <div class="text-center"></div>
       <v-card>
         <v-card-title class="headline"
           >Welcome to the Vuetify + Nuxt.js template</v-card-title
@@ -60,13 +57,7 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  components: {}
 }
 </script>
